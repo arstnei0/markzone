b42bd7cf920f1a037943a1563883b5e89c147858
Zihan Chen <me@zihan.ga>
2023-01-10 18:32:11 +0800

    Change adapter to node

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import solid from "solid-start/vite"
 import { defineConfig } from "vite"
 // @ts-expect-error no typing
 import vercel from "solid-start-vercel"
+// @ts-expect-error no typing
+import node from "solid-start-node"
 
 export default defineConfig(() => {
 	return {
-		plugins: [solid({ ssr: true })],
+		plugins: [solid({ ssr: true, adapter: node() })],
 		ssr: {
 			external: ["@prisma/client"],
 		},
