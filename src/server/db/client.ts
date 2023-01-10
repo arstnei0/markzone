860b3b650de81727ea2932e13049b2e27281888b
Zihan Chen <me@zihan.ga>
2023-01-10 17:49:46 +0800

    Change adapter to node

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,3 +1,4 @@
+// import { PrismaClient } from "../../../prisma/client"
 import { PrismaClient } from "@prisma/client"
 import { serverEnv } from "~/env/server"
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import solid from "solid-start/vite"
 import { defineConfig } from "vite"
 // @ts-expect-error no typing
 import vercel from "solid-start-vercel"
+// @ts-expect-error no typing
+import node from "solid-start-node"
 
 export default defineConfig(() => {
 	return {
-		plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
+		plugins: [solid({ ssr: true, adapter: node() })],
 		build: {
 			rollupOptions: {},
 		},
-		ssr: { noExternal: ["@prisma/client"] },
 	}
 })
