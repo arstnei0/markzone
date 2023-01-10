891b65d5cee51ddabd6416a34c8e6d1fe97cda81
Zihan Chen <me@zihan.ga>
2023-01-10 17:52:15 +0800

    Change adpater back to node and update prisma output

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import solid from "solid-start/vite"
 import { defineConfig } from "vite"
 // @ts-expect-error no typing
 import vercel from "solid-start-vercel"
-// @ts-expect-error no typing
-import node from "solid-start-node"
 
 export default defineConfig(() => {
 	return {
-		plugins: [solid({ ssr: true, adapter: node() })],
+		plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
 		build: {
 			rollupOptions: {},
 		},
